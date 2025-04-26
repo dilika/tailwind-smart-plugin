7935cb7468f3957a47bf31386d8b32dab9b4badf
dilika <modiboshs@gmail.com>
2025-04-26 18:04:40 +0000

    build error debugging

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,26 +1,15 @@
+rootProject.name = "tailwind-smart-plugin"
+
+// Configure Gradle Enterprise
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("com.gradle.enterprise") version "3.16.1"
 }
-
-rootProject.name = "tailwind-smart-plugin"
 
 // Configure Gradle Enterprise Plugin
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
-    }
-}
-
-// Use Gradle 8.5 for this project
-gradle.rootProject {
-    buildscript {
-        configurations.classpath {
-            resolutionStrategy.capabilitiesResolution.withCapability("org.gradle:gradle-enterprise-gradle-plugin") {
-                selectHighestVersion()
-            }
-        }
     }
 }
 
